486a62e4711131570217ce6587ab50e741d6e680
Maxence Chevee <max.rothen@hotmail.fr>
2023-10-03 11:06:52 +0200

    Prénoms couleurs

---
+++ node.js
@@ -0,0 +1,14 @@
+import chalk from "chalk";
+console.log(
+    chalk.blue("Calypso")
+)
+console.log(
+    chalk.red("Maxence")
+
+)
+console.log(
+    chalk.green("Alexis")
+)
+console.log(
+    chalk.yellow("Jean-Louis")
+)
